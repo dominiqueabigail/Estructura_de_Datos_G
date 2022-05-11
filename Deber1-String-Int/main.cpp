708caa657a6bdc684d60c3df22014dd71235dcad
AguasGustavo <93296823+AguasGustavo@users.noreply.github.com>
2022-05-11 07:36:31 -0500

    Update main.cpp

--- Deber1-String-Int/main.cpp
+++ Deber1-String-Int/main.cpp
@@ -3,11 +3,11 @@
 //
 //  EL PROGRAMA TRNASFORMA UN STRING INGRESADO A INT
 //
-//  AUTORES:
+//  AUTORES: Gustavo Aguas, Carlos Ipiales, Dominique Abigaíl
 //  FECHA DE CREACION: 10/05/2022
 //  FECHA DE MODIFICACION: 10/05/2022
-//  GRUPO:
-//  GITHUB:
+//  GRUPO:2 
+//  GITHUB:dominiqueabigail/Estructura_de_Datos_G
 //
 *///////////////////////////////////////////////////////////////////////////////////////////////
 
